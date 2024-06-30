56b34515c9baac3f9c4720b6439ef2940d4c03e8
Conner Ohnesorge <connerohnesorge@Conners-MacBook-Air.local>
2024-06-29 21:41:09 -0500

    update doc.go for semanticrouter

--- doc.go
+++ doc.go
@@ -1,8 +1,12 @@
-// Package semanticrouter is a package for abstractly computing similarity scores between a query vector embedding and a set of vector embeddings.
+// Package semanticrouter is a package for abstractly computing similarity
+// scores between a query vector embedding and a set of vector embeddings.
 //
-// It provides a simple key-value store for embeddings and a router that can be used to find the best route for a given utterance.
+// It provides a simple key-value store for embeddings and a router that can be
+// used to find the best route for a given utterance.
 //
-// The router uses a similarity score to determine the best route for a given utterance.
+// The router uses a similarity score to determine the best route for a given
+// utterance.
 //
-// The semantic router is designed to be used in conjunction with LLMs and agents to provide a superfast decision-making layer.
+// The semantic router is designed to be used in conjunction with LLMs and
+// agents to provide a superfast decision-making layer.
 package semanticrouter
