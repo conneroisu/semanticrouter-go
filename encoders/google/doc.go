0e24cd312a40f1eb2b786aaeadd32a402ccfff74
Conner Ohnesorge <connerohnesorge@Conners-MacBook-Air.local>
2024-06-29 19:21:45 -0500

    added documentation for google encoder

---
+++ encoders/google/doc.go
@@ -0,0 +1,4 @@
+// Package encoders provides encoders for Google language models.
+//
+// Google language models are language models that is trained on a large corpus of text data.
+package encoders
