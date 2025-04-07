bad71e579aaacd5fe033362b1ad57a179f7b6bd8
connerohnesorge <conneroisu@outlook.com>
2025-04-07 18:52:05 -0500

    latest

--- scripts/makefile/docs.sh
+++
@@ -1,6 +0,0 @@
-# name: makefile.docs.sh
-# description: A script to generate the go docs for the cse-ncaa project.
-# 
-# Usage: make docs
-
-gomarkdoc -o README.md -e .
--- scripts/taskfile/tidy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# file: makefile/tidy.sh
-# title: makefile/tidy.sh
-# description: A script to run go mod tidy and go fmt
-#
-# Usage: make tidy
-
-go mod tidy
